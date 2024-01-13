3b10f300443c0540849b38876c88b4c22a8f0d71
Tobias Pleyer <tpleyer@heilmannsoftware.de>
2024-01-13 23:16:43 +0100

    Reset repo

--- nix/package-dummy/nixpkgs/default.nix
+++
@@ -1,6 +0,0 @@
-args@{ ... }:
-
-let
-  nixpkgs = (import <nixpkgs> {}).fetchFromGitHub (builtins.fromJSON (builtins.readFile ./github.json));
-in
-  import nixpkgs (args // { overlays = [ (import ../haskell/overlay.nix) ]; })
